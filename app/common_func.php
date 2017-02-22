4def11c0cb9b6faeb0eec9d69a9d858268625398
sergiobelya <seriobelya@gmail.com>
2017-02-22 23:27:33 +0200

    uri

--- app/common_func.php
+++ app/common_func.php
@@ -9,3 +9,8 @@ function appPath()
 {
     return __DIR__ . DIRECTORY_SEPARATOR;
 }
+
+function uri($uri)
+{
+    return '/' . trim($uri, '/');
+}
--- app/views/partials/header-nav.php
+++ app/views/partials/header-nav.php
@@ -1,13 +1,13 @@
 
-            <nav class="navbar navbar-fixed-top header">
-                <div class="col-md-12 col-sm-12">
-                    <div class="navbar-header">
-                        <a href="/" class="navbar-brand">Задачник</a>
-                    </div>
-                    <div class="collapse navbar-collapse" id="navbar-collapse1">
-                        <ul class="nav navbar-nav navbar-right">
-                            <li><a href="login"><i class="glyphicon glyphicon-user"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+<nav class="navbar navbar-fixed-top header">
+    <div class="col-md-12 col-sm-12">
+        <div class="navbar-header">
+            <a href="/" class="navbar-brand">Задачник</a>
+        </div>
+        <div class="collapse navbar-collapse" id="navbar-collapse1">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="<?= uri('login') ?>"><i class="glyphicon glyphicon-user"></i></a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
