95fe43df39ad18aa1bf9f427a085ec2bb15e366c
sergiobelya <seriobelya@gmail.com>
2017-02-27 01:43:16 +0200

    mvc-app - errors

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,49 +3,56 @@
 use sergiobelya\TestTaskmanager\lib\Request;
 
 require 'common_func.php';
-require 'boot_libs.php';
 
-// Init Request
-$request = new Request();
+try {
 
-// Dispatch URI
-$dispatcher = include 'routes.php';
-// Fetch method and URI from somewhere
-$http_method = filter_input(INPUT_SERVER, 'REQUEST_METHOD');
-$uri = filter_input(INPUT_SERVER, 'REQUEST_URI');
-// Strip query string (?foo=bar) and decode URI
-if (false !== $pos = strpos($uri, '?')) {
-    $uri = substr($uri, 0, $pos);
-}
-$uri = rawurldecode($uri);
-$route_info = $dispatcher->dispatch($http_method, $uri);
-switch ($route_info[0]) {
-    case FastRoute\Dispatcher::NOT_FOUND:
-        // ... 404 Not Found
-        error404();
-        break;
-    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
-        // ... 405 Method Not Allowed
-        $allowed_methods = $route_info[1];
-        header('HTTP/1.1 405 Method Not Allowed');
-        header("Allow: $allowed_methods");
-        echo 'METHOD NOT ALLOWED';
-        break;
-    case FastRoute\Dispatcher::FOUND:
-        // ... call $handler with $vars
-        $handler = $route_info[1];
-        $vars = $route_info[2];
-        $pos_action_delim = strpos($handler, '::');
-        if ($pos_action_delim) {
-            $controller_name = substr($handler, 0, $pos_action_delim);
-            $action = substr($handler, $pos_action_delim + 2);
-            $request->setRouteParams($vars);
-            $request->setController($controller_name);
-            $request->setAction($action);
-            $response = $request->exec();
-            echo $response->sendHeaders()->getBody();
-        } else {
-            throw new Exception('handler format - FooController::bar');
-        }
-        break;
+    require 'boot_libs.php';
+
+    // Init Request
+    $request = new Request();
+
+    // Dispatch URI
+    $dispatcher = include 'routes.php';
+    // Fetch method and URI from somewhere
+    $http_method = filter_input(INPUT_SERVER, 'REQUEST_METHOD');
+    $uri = filter_input(INPUT_SERVER, 'REQUEST_URI');
+    // Strip query string (?foo=bar) and decode URI
+    if (false !== $pos = strpos($uri, '?')) {
+        $uri = substr($uri, 0, $pos);
+    }
+    $uri = rawurldecode($uri);
+    $route_info = $dispatcher->dispatch($http_method, $uri);
+    switch ($route_info[0]) {
+        case FastRoute\Dispatcher::NOT_FOUND:
+            // ... 404 Not Found
+            error404();
+            break;
+        case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+            // ... 405 Method Not Allowed
+            $allowed_methods = $route_info[1];
+            header('HTTP/1.1 405 Method Not Allowed');
+            header("Allow: $allowed_methods");
+            echo 'METHOD NOT ALLOWED';
+            break;
+        case FastRoute\Dispatcher::FOUND:
+            // ... call $handler with $vars
+            $handler = $route_info[1];
+            $vars = $route_info[2];
+            $pos_action_delim = strpos($handler, '::');
+            if ($pos_action_delim) {
+                $controller_name = substr($handler, 0, $pos_action_delim);
+                $action = substr($handler, $pos_action_delim + 2);
+                $request->setRouteParams($vars);
+                $request->setController($controller_name);
+                $request->setAction($action);
+                $response = $request->exec();
+                $response->sendHeaders();
+                echo $response->getBody();
+            } else {
+                throw new Exception('handler format - FooController::bar');
+            }
+            break;
+    }
+} catch (Exception $e) {
+    fatalExceptionHandler($e);
 }
--- app/common_func.php
+++ app/common_func.php
@@ -11,6 +11,13 @@ function appPath()
     return __DIR__ . DIRECTORY_SEPARATOR;
 }
 
+if (!function_exists('e')) {
+    function e($str)
+    {
+        return htmlentities($str, ENT_QUOTES, 'UTF-8', false);
+    }
+}
+
 function uri($uri)
 {
     return '/' . trim($uri, '/');
@@ -29,4 +36,22 @@ function uriStatic($url)
 function error404($msg = '')
 {
     throw new Http404Exception($msg);
+}
+
+function fatalExceptionHandler(Exception $e)
+{
+    header('HTTP/1.1 500 Server Error');
+    if ('development' == getenv('APP_ENV')) {
+        echo '<pre>';
+        echo '<b>Fatal error</b>:  Uncaught exception ' . get_class($e) . PHP_EOL;
+        if ($msg = $e->getMessage()) {
+            echo 'with message ' . $msg . PHP_EOL;
+        }
+        echo 'in ' . $e->getFile() . ':' . $e->getLine() . PHP_EOL;
+        echo 'Stack trace:' . PHP_EOL;
+        echo $e->getTraceAsString();
+        echo '</pre>';
+    } else {
+        die('Unknown server error');
+    }
 }
